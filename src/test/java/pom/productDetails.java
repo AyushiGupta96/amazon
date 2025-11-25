9dddc573dc60ad2f3996cf613894fe4db5797c9d
Ayushi Gupta <ayushiguota4050@gmail.com>
2025-11-24 22:30:44 -0800

    product details

--- src/test/java/pom/productDetails.java
+++ src/test/java/pom/productDetails.java
@@ -1,7 +1,10 @@
 package pom;
 
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class productDetails {
     @FindBy(id="twotabsearchtextbox")
@@ -10,6 +13,17 @@ public class productDetails {
     WebElement Sproduct;
     @FindBy(xpath = "//a[@class='a-link-normal btfSubNavDesktopCustomerReviewsTab sub-nav-desktop-tab']")
     WebElement review;
-
+    public  void search(String name){
+        search.sendKeys(name+ Keys.ENTER);
+    }
+    public void select(){
+        Sproduct.click();
+    }
+    public void GetReview(){
+        review.click();
+    }
+    public productDetails(EdgeDriver driver){
+        PageFactory.initElements(driver,this);
+    }
 
 }
