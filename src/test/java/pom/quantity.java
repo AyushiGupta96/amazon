5b18b4fb24bb6e883dedc13339cc63ec7e97e54a
Ayushi Gupta <ayushiguota4050@gmail.com>
2025-12-01 22:45:55 -0800

    quantity

---
+++ src/test/java/pom/quantity.java
@@ -0,0 +1,40 @@
+package pom;
+
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class quantity {
+    @FindBy(id = "twotabsearchtextbox")
+    WebElement searchbox;
+    @FindBy(xpath = "(//div[@class='a-section aok-relative s-image-fixed-height'][1]")
+    WebElement Sproduct;
+    @FindBy(id = "add-to-cart-button")
+    WebElement addCart;
+    @FindBy(xpath = "//span[@id='a-autoid-34']")
+    WebElement GoTocart;
+    @FindBy(xpath= "(//input[@class='a-color-link'])[1]")
+    WebElement removeproduct;
+   public void search(String name){
+       searchbox.sendKeys(name+ Keys.ENTER);
+   }
+   public void product(){
+       Sproduct.click();
+   }
+   public void add2cart(){
+       addCart.click();
+   }
+   public void Go2cart(){
+       GoTocart.click();
+   }
+   public void remove(){
+       removeproduct.click();
+   }
+   public quantity(EdgeDriver driver){
+       PageFactory.initElements(driver,this);
+   }
+
+
+}
