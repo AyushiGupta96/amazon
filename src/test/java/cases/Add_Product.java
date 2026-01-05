b22a39ccbd570dd61e8ae32d19bffb762bed79db
Ayushi Gupta <ayushiguota4050@gmail.com>
2026-01-05 15:52:07 -0800

    Adding cart

--- src/test/java/cases/Add_Product.java
+++ src/test/java/cases/Add_Product.java
@@ -1,5 +1,10 @@
 package cases;
 
-public class Add_Product {
+import pom.AddingCart;
 
+public class Add_Product extends start{
+   public void productadd(){
+       AddingCart a = new AddingCart(driver);
+
+   }
 }
--- src/test/java/pom/AddingCart.java
+++ src/test/java/pom/AddingCart.java
@@ -9,10 +9,20 @@ import org.openqa.selenium.support.PageFactory;
 import java.security.Key;
 
 public class AddingCart {
+    @FindBy(id = "twotabsearchtextbox")
+    WebElement search;
     @FindBy(id ="add-to-cart-button")
     WebElement cart;
+    @FindBy(xpath = "(//div[@class='a-section aok-relative s-image-fixed-height'])[1]")
+    WebElement selectP;
+    public void search(String name){
+    search.sendKeys(name+Keys.ENTER);
+    }
     public void addCart(){
-        cart.sendKeys(Keys.ENTER);
+        cart.click();
+    }
+    public void sp(){
+        selectP.click();
     }
     public AddingCart(EdgeDriver driver){
         PageFactory.initElements(driver,this);
