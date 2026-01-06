dac9e5fbe5ec93d7ae494b2984215bbac8aeb255
Ayushi Gupta <ayushiguota4050@gmail.com>
2026-01-05 16:07:10 -0800

    Add_product

--- src/test/java/cases/Add_Product.java
+++ src/test/java/cases/Add_Product.java
@@ -2,9 +2,19 @@ package cases;
 
 import pom.AddingCart;
 
+import java.util.Iterator;
+import java.util.Set;
+
 public class Add_Product extends start{
    public void productadd(){
        AddingCart a = new AddingCart(driver);
-
+       a.search("pen");
+       a.sp();
+       Set<String> ss = driver.getWindowHandles();
+       Iterator<String> it = ss.iterator();
+       String parent = it.next();
+       String child = it.next();
+       driver.switchTo().window(child);
+       a.addCart();
    }
 }
